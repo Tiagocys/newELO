a47da8ff65ba838c30eccf6204ab1a3d32998249
Tiagocys <cysneirostiago@gmail.com>
2024-06-09 20:49:06 -0300

    lol

---
+++ app.js
@@ -0,0 +1,9 @@
+var nome = document.getElementById('nome');
+var email = document.getElementById('email');
+var conta = document.getElementById('conta');
+
+window.onload = function(){
+    nome.value = '';
+    email.value = '';
+    conta.value = '';
+}
